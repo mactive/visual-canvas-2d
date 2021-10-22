5c11eac2cf2332aeb556ede09f6b05e33a1de251
mactive <mactive@gmail.com>
2021-10-22 12:00:33 +0800

    toGraphviz

--- src/tree/graphTree.tsx
+++ src/tree/graphTree.tsx
@@ -26,30 +26,30 @@ const traverse = (node: IBinaryTreeNode, visitFn: any, depth: number, orderType 
 
     switch (orderType) {
         case ORDER.IN_ORDER:
-          if (typeof node !== "number") {
-            traverse(node.left!, visitFn, depth + 1, orderType);
-          }
-          visitFn(node, depth);
-    
-          if (typeof node !== "number") {
-            traverse(node.right!, visitFn, depth + 1, orderType);
-          }
-          return;
+            if (node !== null) {
+                traverse(node.left!, visitFn, depth + 1, orderType);
+            }
+            visitFn(node, depth);
+
+            if (node !== null) {
+                traverse(node.right!, visitFn, depth + 1, orderType);
+            }
+            return;
         case ORDER.PRE_ORDER:
-          visitFn(node, depth);
-          if (node !== null) {
-            traverse(node.left!, visitFn, depth + 1, orderType);
-            traverse(node.right!, visitFn, depth + 1, orderType);
-          }
-          return;
+            visitFn(node, depth);
+            if (node !== null) {
+                traverse(node.left!, visitFn, depth + 1, orderType);
+                traverse(node.right!, visitFn, depth + 1, orderType);
+            }
+            return;
         case ORDER.POST_ORDER:
-          if (typeof node !== "number") {
-            traverse(node.left!, visitFn, depth + 1, orderType);
-            traverse(node.right!, visitFn, depth + 1, orderType);
-          }
-          visitFn(node, depth);
-          return;
-      }
+            if (node !== null) {
+                traverse(node.left!, visitFn, depth + 1, orderType);
+                traverse(node.right!, visitFn, depth + 1, orderType);
+            }
+            visitFn(node, depth);
+            return;
+    }
 }
 
 class BinaryTree {
@@ -80,9 +80,10 @@ class BinaryTree {
         let str = "";
 
         const addNodeToStr = (node: IBinaryTreeNode, depth: number): void => {
-            const nodeValue = typeof node === "number" ? node : node.value;
-            str +=
-                str.length === 0 ? nodeValue : `\n${" ".repeat(depth)} -> ${nodeValue}`;
+            if(node !== null) {
+                str +=
+                str.length === 0 ? node.value : `\n${"*".repeat(depth)} -> ${node.value}`;
+            }
         };
 
         traverse(this, addNodeToStr, 0, orderType);
@@ -95,34 +96,68 @@ class BinaryTree {
         traverse(
             this,
             (node: IBinaryTreeNode) => {
-                if(node !== null) {
+                if (node !== null) {
                     sum += node.value!;
                 }
-                // const nodeValue = typeof node === "object" ? node.value : node.value;
             },
             0,
         );
         return sum;
     }
+
+    /**
+     * 
+     * @returns        
+     * 6 -- 4;6 -- 7;8 -- 6;8 -- 22;22 -- 12;22 -- 44;
+     */
+    toGraphviz = (): string => {
+        let str = ""
+
+        const addNodeToStr = (node: IBinaryTreeNode, depth: number) => {
+          const nodeValue = (n: IBinaryTreeNode) => n !== null ? n.value : null;
+          
+          if(node instanceof BinaryTree && node !== null) {
+            str += nodeValue(node.left!) !== null ? 
+                `${node.value} -- ${nodeValue(node.left!)};` : '';
+            str += nodeValue(node.right!) !== null ? 
+                `${node.value} -- ${nodeValue(node.right!)};` : '';
+          }
+    
+        };
+        
+        traverse(this, addNodeToStr, 0, ORDER.IN_ORDER);
+    
+        return str
+      }
 }
 
 
 const GraphTree: React.FC = () => {
 
     const tree = new BinaryTree(8);
-    tree.addLeft(new BinaryTree(6).addLeft(new BinaryTree(1)).addRight(new BinaryTree(4)))
-    tree.addRight(new BinaryTree(15).addLeft(new BinaryTree(10)).addRight(new BinaryTree(17)))
+    tree.addLeft(new BinaryTree(6).addLeft(new BinaryTree(4)).addRight(new BinaryTree(7)))
+    tree.addRight(new BinaryTree(22).addLeft(new BinaryTree(12)).addRight(new BinaryTree(44)))
+
+    
 
     const [total, setTotal] = useState(tree.total())
-
+    const [graphviz, setGraphviz] = useState(tree.toGraphviz())
+    const [preOrder, setpreOrder] = useState(tree.printTree(ORDER.PRE_ORDER))
+    const [postOrder, setpostOrder] = useState(tree.printTree(ORDER.POST_ORDER))
+    const [inOrder, setinOrder] = useState(tree.printTree(ORDER.IN_ORDER))
+    
     return (
         <pre>
+            {graphviz}
             < Graphviz dot={`graph {
-
+                ${graphviz}
             }`} />
             <h2>Preorder</h2>
+            {preOrder}
             <h2>Postorder</h2>
+            {postOrder}
             <h2>Inorder</h2>
+            {inOrder}
             <hr />
             <b>Total: {total}</b>
 
